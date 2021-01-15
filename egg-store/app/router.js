4232f09c08588d2ae7e0aab712e4c2ed675b17cc
林云富 <lin13177896353@163.com>
2021-01-15 15:23:13 +0800

    #000路由当中获取中间件配置

--- egg-store/app/router.js
+++ egg-store/app/router.js
@@ -4,7 +4,9 @@
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
+  //  路由当中获取中间件
+  var auth=app.middleware.auth({title:'this is router.js  middleware'})
   const { router, controller } = app;
-  router.get('/', controller.home.index);
+  router.get('/', auth,controller.home.index);
   router.post('/add', controller.home.add);
 };
