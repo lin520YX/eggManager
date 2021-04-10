30c2694559219a00b1a8b2cc082f863a786657c8
lyf <lin13177896353@163.com>
2021-04-11 00:54:54 +0800

    #000 service

--- egg-store/app/router.js
+++ egg-store/app/router.js
@@ -9,4 +9,5 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/', auth,controller.home.index);
   router.post('/add', controller.home.add);
+  router.get('/news',controller.news.index);
 };
