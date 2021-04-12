717ea0af2c9820a6956303e9a000d7fe4ee47fb8
lyf <lin13177896353@163.com>
2021-04-12 22:56:47 +0800

    egg 中cookie的用法

--- egg-store/app/controller/home.js
+++ egg-store/app/controller/home.js
@@ -6,25 +6,27 @@ class HomeController extends Controller {
     const { ctx } = this;
     console.log(this.ctx.request.body)
     // 设置cookies
-    ctx.cookies.set('key','value')
+    // ctx.cookies.set('key','value')
 
     // egg 设置cookies的参数option
 
     ctx.cookies.set('key','value',{
       maxAge:24*3600*1000,
-      httpOnly:true, //仅仅在服务端显示 在客户端中不展示
-      encrypt:true //cookie 加密
+      // httpOnly:true, //仅仅在服务端显示 在客户端中不展示
+      encrypt:true, //cookie 加密
+      signed:false
     })
    
     await this.ctx.render('home')
   }
   async add() {
     const { ctx } = this;
-    console.log(this.ctx.request.body)
+    console.log('111',ctx.cookies.get('key'))
+    console.log('1112',this.ctx.request.body)
      // 获取cookies
-     ctx.cookies.get('key')
+    
     // 清空cookies 
-    ctx.cookies.set('key',null)
+    // ctx.cookies.set('key',null)
     ctx.body = 'hi, egg';
   }
   // session 是另外一种记录客户状态的机制 不同的是cookie保存在客户端浏览器中，而session 保存在服务器上
--- egg-store/app/middleware/auth.js
+++ egg-store/app/middleware/auth.js
@@ -1,7 +1,6 @@
 module.exports = (options,app)=>{
     return async function auth(ctx,next){
         console.log('options',options)
-        // CSRF 攻击：伪造用户请求向网站发起恶意请求
         ctx.state.csrf = ctx.csrf
         await next()
     }
